9a5930126b2f5f970a698974c1dc0fe90a24a5e4
taku1417 <58448175+taku1417@users.noreply.github.com>
2022-11-07 04:52:44 +0900

    Update test.js

--- nullpo/command/test/test.js
+++ nullpo/command/test/test.js
@@ -2,7 +2,7 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS]});
 const svid = '966674976956645407',ncsvid = '1015585928779137105';
 const tex_test = '980304691604881419', tex_nlpcs_nofi = '1015852168810606592';
-const channelTest = client.guilds.cache.get(svid).channels.cache.get(tex_test);
+const channelTest = client./*guilds.cache.get(svid).*/channels.cache.get(tex_test);
 const channelncnofi = client.guilds.cache.get(ncsvid).channels.cache.get(tex_nlpcs_nofi);
 
 function test(interaction) {
