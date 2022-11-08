8172ee9978d8d2bb3d067e8c94816ee9989ef54e
taku1417 <58448175+taku1417@users.noreply.github.com>
2022-11-09 02:57:39 +0900

    fix

--- nullpo/components/voiceinout.js
+++ nullpo/components/voiceinout.js
@@ -1,6 +1,6 @@
 const { Client, Intents } = require('discord.js');
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_VOICE_STATES]});
 const logger = require('../log/logger.js');
-const tex_jllog = '978962695418155019',tex_jihou = '997274370122731611',tex_pjsekai = '999675995936280717';
-const vc_atumare = '997274624045879407',vc_pjsekai = '981173824294879322',vc_apex = '992161885862502400',vc_music = '982523943309180978',vc_spla = '1017431011442819142';
+const tex_jihou = '997274370122731611';
+
 
