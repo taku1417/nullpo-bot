7596df4de48d7e4d9228cafd5395f79132473d75
taku1417 <58448175+taku1417@users.noreply.github.com>
2022-11-09 00:40:39 +0900

    fix schedule

--- nullpo/components/schedule.js
+++ nullpo/components/schedule.js
@@ -3,7 +3,7 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_
 const cron = require('node-cron');
 const channeljihou = client.channels.cache.get(tex_jihou);
 const channelncnofi = client.channels.cache.get(tex_nlpcs_nofi);
-const channeltest = client.channels.cache.get('979084899703218186');
+
         
 function cron_schedule() {
 
