49bf00fddfd2d079c4952dff013afb0dae1812cd
taku1417 <58448175+taku1417@users.noreply.github.com>
2023-02-22 19:47:20 +0900

    fix

--- nullpobot.js
+++ nullpobot.js
@@ -451,7 +451,34 @@ if (interaction.commandName === 'mori') {
 	if (interaction.customId === 'no') no_button(interaction);
 });
 client.on('messageDelete', message => {
-	delete_logger(message);
+	const Month = new Date().getMonth()+1,Day = new Date().getDate(),Hour = new Date().getHours(),Min = new Date().getMinutes(),Sec = new Date().getSeconds(),MilliSec = new Date().getMilliseconds(),Hour0 = ('0' + Hour).slice(-2),Min0 = ('0' + Min).slice(-2),Sec0 = ('0' + Sec).slice(-2),MilliSec0 = ('00' + MilliSec).slice(-3);
+        //client.channels.cache.get(nullpo_admin_log).send(`[delete_logger]実行されています。`);
+        const embed = {
+                title: 'メッセージ削除',
+                color: 0xff0000,
+                description: '削除されたメッセージです。',
+                fields: [{
+                        name: 'メッセージ内容',
+                        value: message.content,
+                },{
+                        name: 'チャンネル',
+                        value: (message.channel != null ? message.channel : 不明),
+                }],
+                footer: {
+                        text: '削除者: ' + message.author.tag + ' | ' + Month + '/' + Day + ' ' + Hour0 + ':' + Min0 + ':' + Sec0 + '.' + MilliSec0,
+                },
+        };
+        switch(message.guild.id) {
+                case nullpo_server_id:
+                        client.guilds.cache.get(nullpo_server_id).channels.cache.get(nullpo_admin_log).send({embeds: [embed]});
+                        break;
+                case nullpo_casino_server_id:
+                        client.guilds.cache.get(nullpo_casino_server_id).channels.cache.get(nullpocasino).send({embeds: [embed]});
+                        break;
+                default:
+                        break;
+        }
+	//delete_logger(message);
 });
 client.once('ready', () => {
 	client.channels.cache.get(tex_dblog).send('ぬるぽbotが起動しました。');//デバッグ鯖のログに流れる
