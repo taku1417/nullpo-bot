de38d6cb737fe50e3f17dcf81d3f74f47d347407
taku1417 <taku1417@nullpo-mc.com>
2023-10-04 23:39:01 +0900

    [feat] pingコマンドを実装。
    WebSocket pingとAPI Endpoint pingの値を確認可能。
    実行時に軽い説明を載せています。

---
+++ nullpo/SlashCommand/ping.js
@@ -0,0 +1,30 @@
+const e = require('express');
+const logger = require('../log/logger.js');
+const { Client, SlashCommandBuilder, ApplicationCommandType, Interaction, ChatInputCommandInteraction } = require('discord.js');
+
+module.exports = {
+    data : new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('ping値を返します。'),
+    /**
+     * ping値をinteraction.reply()で返します
+     * @param {Interaction<ChatInputCommandInteraction>} interaction
+     * @param {Client} client
+     * @returns {void}
+     */
+    async execute(interaction, client) {
+        logger('command');
+        await interaction.reply({
+            content: `pong!\nWebSocket Ping: ${client.ws.ping}ms\nAPI Endpoint Ping: 計測中...\n
+            WebSocket PingはDiscord APIとぬるぽbot間の応答速度。ぬるぽbotはアメリカのサーバー上で実行しているため良い傾向にあります。\n
+            API Endpoint Pingはコマンドを実行しbotの処理が開始されてから、このメッセージが作成されるまでの時間。読み上げbotの計測方法と同じです。\n負荷のかかる処理を行っていたり、Discord APIに障害が起きていたりすると値が大きくなります。`,
+            ephemeral: true
+        });
+        let msg = await interaction.fetchReply();
+        await interaction.editReply({
+            content: `pong!\nWebSocket Ping: ${client.ws.ping}ms\nAPI Endpoint Ping: ${msg.createdTimestamp - interaction.createdTimestamp}ms\n
+            WebSocket PingはDiscord APIとぬるぽbot間の応答速度。ぬるぽbotはアメリカのサーバー上で実行しているため良い傾向にあります。\n
+            API Endpoint Pingはコマンドを実行しbotの処理が開始されてから、このメッセージが作成されるまでの時間。読み上げbotの計測方法と同じです。\n負荷のかかる処理を行っていたり、Discord APIに障害が起きていたりすると値が大きくなります。`
+        });
+    }
+};
