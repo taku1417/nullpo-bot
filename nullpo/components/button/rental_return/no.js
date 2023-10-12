d363f3402adaecf2c45b3f2596964ad71c0dc1cb
taku1417 <taku1417@nullpo-mc.com>
2023-10-12 19:58:25 +0900

    [fix]よくみたら二個あった

--- nullpo/components/button/rental_return/no.js
+++ nullpo/components/button/rental_return/no.js
@@ -22,26 +22,3 @@ module.exports = {
                 no_button(interaction);
         }
 }
-const { ButtonBuilder, ButtonStyle } = require("discord.js");
-
-function no_button(interaction) {
-        if (lendSystemCurrent != '') {
-                interaction.reply({
-                        content: "キャンセルしました。",
-                        ephemeral: true,
-                        fetchReply: true
-                });
-                lendSystemCurrent = '';
-                lendSystemMode = '';
-        }
-}
-
-module.exports = {
-        data: new ButtonBuilder()
-                .setCustomId('no')
-                .setLabel('いいえ')
-                .setStyle(ButtonStyle.Danger),
-        async execute(interaction, client) {
-                no_button(interaction);
-        }
-}
