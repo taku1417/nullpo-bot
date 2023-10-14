27f276c20ff8fd69a9ad453f0425e1aef75e3f6c
taku1417 <taku1417@nullpo-mc.com>
2023-10-14 11:28:22 +0900

    [fix]ピン留め出来ないチャンネルでピン留めしようとしてエラーを起こしていた問題を修正。

--- nullpo/components/appCommand/pinned.js
+++ nullpo/components/appCommand/pinned.js
@@ -1,14 +1,23 @@
-const { ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
+const { ContextMenuCommandBuilder, ApplicationCommandType, Message, ContextMenuCommandInteraction, Interaction } = require('discord.js');
 module.exports = {
     data: new ContextMenuCommandBuilder()
             .setName('ピン留めをする/解除する')
             .setType(ApplicationCommandType.Message),
+	/**
+	 * 
+	 * @param {ContextMenuCommandInteraction} interaction 
+	 * @returns 
+	 */
     async execute(interaction) {
 		logger.trace("[appCmd] pinned.js");
 		const message = interaction.options.getMessage('message');
 		if (message.system) {
 			logger.trace("[appCmd] pinned.js: system message detected. returning...");
 			return interaction.reply({ content: "システムメッセージはピン留めできません", ephemeral: true });
+		}
+		if (message.channel.type == ( 2 || 4 || 13 || 14 )) {
+			logger.trace("[appCmd] pinned.js: cannot pin message channeltype. returning...");
+			return interaction.reply({ content: "VC内チャットやステージ内チャットなど、ピン留めが出来ないチャンネルです。", ephemeral: true });
 		}
 		if (message.pinned) {
 			logger.trace("[appCmd] pinned.js: message is already pinned. unpinning...");
